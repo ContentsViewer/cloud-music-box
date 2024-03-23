6b6eb3fb7c055abf67c35810232fb338eb7d5bce
IOE <fivetwothreesix@gmail.com>
2024-03-23 14:41:08 +0900

    feat: Add pwa config

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+
+import withPWA from "next-pwa";
+
+export default withPWA({
+    dest: "public",
+});
