2fa8e23dd824f6a4476e89173af609a4f384deae
IOE <fivetwothreesix@gmail.com>
2024-04-08 00:34:37 +0900

    fix: Fix manifest link

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="manifest.json" />
       </head>
       <body>
         <AppRouterCacheProvider>
